9cb4cc337111707eb5efa193a0516535be63ae78
Tim S. Biernat <tbiernat@ra.rockwell.com>
2019-06-18 18:01:04 -0500

    code cleanup

--- src/db.ts
+++ src/db.ts
@@ -1,23 +1,10 @@
 import { log } from "winston";
 import { Client } from "pg";
-var os = require('os');
 
 let client: Client;
 
 export async function connect() {
     let host = process.env.DB_HOST;
-    // if (!host) {
-    //     let iface = os.networkInterfaces();
-    //     // local dev machine
-    //     if (iface.en0 && iface.en0[1].address) {
-    //         host = iface.en0[1].address;
-    //     }
-    //     // local pi wifi
-    //     if (iface.wlan0 && iface.wlan0[1].address) {
-    //         host = iface.wlan0[1].address;
-    //     }
-    // }
-    // log("info", "DB_HOST: %s", host);
     client = new Client({
         user: process.env.DB_USER,
         host: host,
